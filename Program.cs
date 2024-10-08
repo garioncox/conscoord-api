de17681ef98b7abd87f79efa444bb5c0e132d6dc
kalin.nebeker <kalin.nebeker@ENGR9HCP5S3>
2024-10-08 17:41:42 -0600

    chron jobs created and work - send emails at midnight for low PSOs

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using conscoord_api.Data;
 using conscoord_api.Data.Interfaces;
 using conscoord_api.Services;
 using Microsoft.EntityFrameworkCore;
+using Coravel;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,6 +23,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Cron Scheduler
+builder.Services.AddScheduler();
+builder.Services.AddScoped<SendEmailsAtMidnight>();
+//this is how you pass in parameters
+//builder.Services.AddTransient<string>(p => "");
+
 // Services
 builder.Services.AddDbContext<PostgresContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("db")));
 builder.Services.AddScoped<ICompanyService, CompanyService>();
@@ -29,6 +36,7 @@ builder.Services.AddScoped<IShiftService, ShiftService>();
 builder.Services.AddScoped<IProjectService, ProjectService>();
 builder.Services.AddScoped<IEmployeeShiftService, EmployeeShiftService>();
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddScoped<EmailController>();
 
 var app = builder.Build();
 
@@ -41,6 +49,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.Services.UseScheduler(scheduler => {
+    //add more of these for different times/different processes
+    scheduler.Schedule<SendEmailsAtMidnight>()
+        .EveryMinute()      
+        //.Cron("0 0 * * *")
+        .PreventOverlapping(nameof(SendEmailsAtMidnight));
+});
 
 app.UseCors(x => x
     .AllowAnyMethod()
--- Services/EmployeeShiftService.cs
+++ Services/EmployeeShiftService.cs
@@ -1,5 +1,6 @@
 ﻿using conscoord_api.Data;
 using conscoord_api.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace conscoord_api.Services;
 
@@ -36,4 +37,17 @@ public class EmployeeShiftService : IEmployeeShiftService
             await _context.SaveChangesAsync();
         }
     }
+
+  public List<EmployeeShift> GetFutureShifts()
+  {
+    DateTime currentTime = DateTime.Now;
+    var futureShifts = _context.EmployeeShifts
+      .Include(s => s.Shift)
+      .Include(e => e.Emp)
+      .AsEnumerable() // Bring the data into memory
+    .Where(s => DateTime.ParseExact(s.Shift.StartTime, "yyyy/MM/dd HH:mm:ss", null) >= currentTime)
+    .ToList();
+
+    return futureShifts;
+  }
 }
