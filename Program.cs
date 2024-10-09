c53545f2c7b4bf013ac387bbfcc47a68a445f76c
Bryce Coon <bryce.coon@students.snow.edu>
2024-10-08 18:35:33 -0600

    [KGB-97] User can see employees

--- Program.cs
+++ Program.cs
@@ -29,6 +29,7 @@ builder.Services.AddScoped<IShiftService, ShiftService>();
 builder.Services.AddScoped<IProjectService, ProjectService>();
 builder.Services.AddScoped<IEmployeeShiftService, EmployeeShiftService>();
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddScoped<IRoleService, RoleService>();
 
 var app = builder.Build();
 
