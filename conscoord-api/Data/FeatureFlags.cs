7ada7020540d191510b06353b5ddfb6b9582d0ea
Garion Cox <garion.cox@students.snow.edu>
2024-10-15 16:33:08 -0600

    Env test TWO

--- conscoord-api/Data/FeatureFlags.cs
+++ conscoord-api/Data/FeatureFlags.cs
@@ -1,5 +1,6 @@
 namespace conscoord_api.Data;
-public class FeatureFlags
+public class CustomConfiguration
 {
+    public required string DB { get; set; }
     public required bool EMAIL_ENABLED { get; set; } = false;
 }
