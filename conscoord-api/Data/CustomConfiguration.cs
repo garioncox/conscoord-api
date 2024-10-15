ed5eecd45c28afdb0c020db421f7ac8eb6c9856c
Garion Cox <garion.cox@students.snow.edu>
2024-10-15 17:22:04 -0600

    Removed SMTPSettings

--- conscoord-api/Data/CustomConfiguration.cs
+++ conscoord-api/Data/CustomConfiguration.cs
@@ -1,6 +1,9 @@
 namespace conscoord_api.Data;
 public class CustomConfiguration
 {
+    public required string SMTP_SENDERNAME { get; set; }
+    public required string SMTP_USERNAME { get; set; }
+    public required string SMTP_PASSWORD { get; set; }
     public required string DB { get; set; }
-    public required bool EMAIL_ENABLED { get; set; } = false;
+    public required bool EMAIL_ENABLED { get; set; }
 }
