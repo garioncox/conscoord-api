79c6f60a53d1aaee61e383fb80f6acb54b5533d9
bryce.coon <bryce.coon@ENGRBHCP5S3>
2024-10-15 19:19:49 -0600

    done

--- Data/Interfaces/IProjectShiftService.cs
+++ Data/Interfaces/IProjectShiftService.cs
@@ -1,6 +1,10 @@
-﻿namespace conscoord_api.Data.Interfaces
+﻿using conscoord_api.Data.DTOs;
+
+namespace conscoord_api.Data.Interfaces;
+
+public interface IProjectShiftService
 {
-    public interface IProjectShiftService
-    {
-    }
+    Task CreateProjectShiftAsync(ProjectShiftDTO projectShift);
+    Task DeleteProjectShiftAsync(int shiftId);
+    Task<List<ProjectShift>> GetAllProjectShifts();
 }
