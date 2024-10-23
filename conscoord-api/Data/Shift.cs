9533c16bc2ad2ba97e84726011c8e9dd62f777a0
kalin.nebeker <kalin.nebeker@ENGR9HCP5S3>
2024-10-22 18:40:06 -0600

    lint

--- conscoord-api/Data/Shift.cs
+++ conscoord-api/Data/Shift.cs
@@ -2,19 +2,19 @@
 
 public partial class Shift
 {
-    public int Id { get; set; }
+  public int Id { get; set; }
 
-    public string StartTime { get; set; } = null!;
+  public string StartTime { get; set; } = null!;
 
-    public string EndTime { get; set; } = null!;
+  public string EndTime { get; set; } = null!;
 
-    public string? Description { get; set; }
+  public string? Description { get; set; }
 
-    public string? Location { get; set; }
+  public string? Location { get; set; }
 
-    public int RequestedEmployees { get; set; }
+  public int RequestedEmployees { get; set; }
 
-    public string Status { get; set; } = null!;
+  public string Status { get; set; } = null!;
 
   public static string STATUS_ACTIVE = "ACTIVE";
   public static string STATUS_ARCHIVED = "ARCHIVED";
@@ -22,5 +22,5 @@ public partial class Shift
 
   public virtual ICollection<EmployeeShift> EmployeeShifts { get; set; } = new List<EmployeeShift>();
 
-    public virtual ICollection<ProjectShift> ProjectShifts { get; set; } = new List<ProjectShift>();
+  public virtual ICollection<ProjectShift> ProjectShifts { get; set; } = new List<ProjectShift>();
 }
