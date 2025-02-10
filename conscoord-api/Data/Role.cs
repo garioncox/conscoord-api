e12c613c98a206b4ce57a0c7574a44197d237492
Garion Cox <garion.cox@students.snow.edu>
2025-02-10 09:14:09 -0700

    Created tests for RoleUtils

--- conscoord-api/Data/Role.cs
+++ conscoord-api/Data/Role.cs
@@ -8,6 +8,9 @@ public partial class Role
     public int Id { get; set; }
 
     public string? Rolename { get; set; }
+    public static string PSO_ROLE = "PSO";
+    public static string CLIENT_ROLE = "CLIENT";
+    public static string ADMIN_ROLE = "ADMIN";
 
     public virtual ICollection<Employee> Employees { get; set; } = new List<Employee>();
 }
