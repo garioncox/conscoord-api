eaaa69f5d882820eb9c1b88f70a0075cc8a2ee4b
Garion Cox <garion.cox@students.snow.edu>
2025-02-18 10:00:07 -0700

    Linted + removed Moq

--- conscoord-tests/Services/EmailServiceTests.cs
+++ conscoord-tests/Services/EmailServiceTests.cs
@@ -1,9 +1,8 @@
 using conscoord_api;
-using conscoord_api.Controllers;
 using conscoord_api.Data;
 using conscoord_api.Data.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using Moq;
+using NSubstitute;
 
 namespace conscoord_tests.Services;
 
@@ -14,10 +13,10 @@ public class ShiftClockInReminderTests
     public async Task Invoke_SendsEmail_WhenNotClockedIn()
     {
         // Arrange
-        var mockShiftService = new Mock<IEmployeeShiftService>();
-        var mockEmailController = new Mock<IEmailService>() { CallBase = true };
+        var mockShiftService = Substitute.For<IEmployeeShiftService>();
+        var mockEmailController = Substitute.For<IEmailService>();
 
-        mockEmailController.Setup(e => e.SendEmail(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+        mockEmailController.SendEmail(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>())
                            .Returns(new OkResult());
 
         var shift = new EmployeeShift
@@ -26,26 +25,26 @@ public class ShiftClockInReminderTests
             ClockInTime = null
         };
 
-        mockShiftService.Setup(s => s.GetShiftsWithinTime(It.IsAny<DateTime>(), It.IsAny<DateTime>()))
+        mockShiftService.GetShiftsWithinTime(Arg.Any<DateTime>(), Arg.Any<DateTime>())
             .Returns(new List<EmployeeShift> { shift });
 
-        var reminder = new ShiftClockInReminder(mockShiftService.Object, mockEmailController.Object);
+        var reminder = new ShiftClockInReminder(mockShiftService, mockEmailController);
 
         // Act
         await reminder.Invoke();
 
         // Assert
-        mockEmailController.Verify(e => e.SendEmail("test@example.com", EmailTemplates.NotClockedIn.Subject, EmailTemplates.NotClockedIn.MailBody), Times.Once);
+        mockEmailController.Received(1).SendEmail("test@example.com", EmailTemplates.NotClockedIn.Subject, EmailTemplates.NotClockedIn.MailBody);
     }
 
     [Test]
     public async Task Invoke_DoesNotSendEmail_WhenClockedIn()
     {
         // Arrange
-        var mockShiftService = new Mock<IEmployeeShiftService>();
-        var mockEmailController = new Mock<IEmailService>() { CallBase = true };
+        var mockShiftService = Substitute.For<IEmployeeShiftService>();
+        var mockEmailController = Substitute.For<IEmailService>();
 
-        mockEmailController.Setup(e => e.SendEmail(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+        mockEmailController.SendEmail(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>())
                        .Returns(new StatusCodeResult(405));
 
         var shift = new EmployeeShift
@@ -54,15 +53,15 @@ public class ShiftClockInReminderTests
             ClockInTime = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")
         };
 
-        mockShiftService.Setup(s => s.GetShiftsWithinTime(It.IsAny<DateTime>(), It.IsAny<DateTime>()))
+        mockShiftService.GetShiftsWithinTime(Arg.Any<DateTime>(), Arg.Any<DateTime>())
             .Returns(new List<EmployeeShift> { shift });
 
-        var reminder = new ShiftClockInReminder(mockShiftService.Object, mockEmailController.Object);
+        var reminder = new ShiftClockInReminder(mockShiftService, mockEmailController);
 
         // Act
         await reminder.Invoke();
 
         // Assert
-        mockEmailController.Verify(e => e.SendEmail(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()), Times.Never);
+        mockEmailController.Received(0).SendEmail(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>());
     }
 }
